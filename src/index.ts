afc15afa4b3269e4966cbb15f4736a29d85d2f54
Daniel Shleifman <dancheskus@gmail.com>
2021-07-29 19:30:16 +0300

    linting and apollo added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log(3)
+import { ApolloServer, gql } from 'apollo-server'
+
+const typeDefs = gql`
+  type Query {
+    helloWorld(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    helloWorld: (_, { name }) => (name ? `Hi, ${name}!` : 'Hello World'),
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
