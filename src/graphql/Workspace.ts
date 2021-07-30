e3fadc83dab2e8da5df341d6f3de70e884921522
Daniel Shleifman <dancheskus@gmail.com>
2021-07-30 21:29:58 +0300

    refactor

--- src/graphql/Workspace.ts
+++ src/graphql/Workspace.ts
@@ -2,32 +2,6 @@ import { mutationField, nonNull, objectType } from 'nexus'
 
 import { prisma } from '../prismaSetup'
 
-export const TodoItem = objectType({
-  name: 'TodoItem',
-  definition: t => {
-    t.nonNull.string('id')
-    t.nonNull.string('title')
-    t.string('description')
-    t.float('cost')
-    t.nonNull.boolean('isCompleted')
-    t.nonNull.string('todoListId')
-  },
-})
-
-export const TodoList = objectType({
-  name: 'TodoList',
-  definition: t => {
-    t.nonNull.string('id')
-    t.string('title')
-    t.nonNull.boolean('isLocked')
-    t.nonNull.string('workspaceId')
-    t.nonNull.list.field('todoItems', {
-      type: 'TodoItem',
-      resolve: ({ id }) => prisma.todoItem.findMany({ where: { todoListId: id } }),
-    })
-  },
-})
-
 export const Workspace = objectType({
   name: 'Workspace',
   definition: t => {
