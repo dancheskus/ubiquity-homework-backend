e9970c1bdb23f54094cf78e952d597669545b7b2
Daniel Shleifman <dancheskus@gmail.com>
2021-08-02 19:58:00 +0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import { v4 as uuidv4 } from 'uuid'
-
 import { server } from './server'
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
 })
-
-// console.log(uuidv4())
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,5 @@ import { schema } from './schema'
 
 export const server = new ApolloServer({
   schema,
-  context: ({ req: { headers } }) => {
-    console.log(headers)
-    return { currentUserId: headers['current-user-id'] }
-  },
+  context: ({ req: { headers } }) => ({ currentUserId: headers['current-user-id'] }),
 })
